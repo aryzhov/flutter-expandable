6d3349911da304013dba9ed84b0aa7f946950f77
Alex Ryzhov <aryzhov07@gmail.com>
2020-01-06 23:26:24 -0800

    #38. Fixed example app to reflect scrolling behavior as in the gif.

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -53,65 +53,58 @@ class Card1 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ExpandableNotifier(
-      child: ScrollOnExpand(
-        scrollOnExpand: false,
-        scrollOnCollapse: true,
-        child: Padding(
-          padding: const EdgeInsets.all(10),
-          child: Card(
-            clipBehavior: Clip.antiAlias,
-            child: Column(
-              children: <Widget>[
-                  SizedBox(
-                  height: 150,
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: Colors.orange,
-                      shape: BoxShape.rectangle,
-                    ),
+      child: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Card(
+          clipBehavior: Clip.antiAlias,
+          child: Column(
+            children: <Widget>[
+                SizedBox(
+                height: 150,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.orange,
+                    shape: BoxShape.rectangle,
                   ),
                 ),
-                ScrollOnExpand(
-                  scrollOnExpand: true,
-                  scrollOnCollapse: false,
-                  child: ExpandableNotifier(
-                    initialExpanded: true,
-                    child: ExpandablePanel(
-                      tapHeaderToExpand: true,
-                      tapBodyToCollapse: true,
-                      theme: ExpandableThemeData(headerAlignment: ExpandablePanelHeaderAlignment.center),
-                      header: Padding(
-                        padding: EdgeInsets.all(10),
-                        child: Text("ExpandablePanel",
-                          style: Theme.of(context).textTheme.body2,
-                        )
-                      ),
-                      collapsed: Text(loremIpsum, softWrap: true, maxLines: 2, overflow: TextOverflow.ellipsis,),
-                      expanded: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          for(var _ in Iterable.generate(5))
-                            Padding(
-                              padding: EdgeInsets.only(bottom: 10),
-                              child: Text(loremIpsum, softWrap: true, overflow: TextOverflow.fade,)
-                            ),
-                        ],
-                      ),
-                      builder: (_, collapsed, expanded) {
-                        return Padding(
-                          padding: EdgeInsets.only(left: 10, right: 10, bottom: 10),
-                          child: Expandable(
-                            collapsed: collapsed,
-                            expanded: expanded,
-                            theme: ExpandableThemeData(crossFadePoint: 0),
-                          ),
-                        );
-                      },
-                    ),
+              ),
+              ScrollOnExpand(
+                scrollOnExpand: true,
+                scrollOnCollapse: false,
+                child: ExpandablePanel(
+                  tapHeaderToExpand: true,
+                  tapBodyToCollapse: true,
+                  theme: ExpandableThemeData(headerAlignment: ExpandablePanelHeaderAlignment.center),
+                  header: Padding(
+                    padding: EdgeInsets.all(10),
+                    child: Text("ExpandablePanel",
+                      style: Theme.of(context).textTheme.body2,
+                    )
                   ),
+                  collapsed: Text(loremIpsum, softWrap: true, maxLines: 2, overflow: TextOverflow.ellipsis,),
+                  expanded: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      for(var _ in Iterable.generate(5))
+                        Padding(
+                          padding: EdgeInsets.only(bottom: 10),
+                          child: Text(loremIpsum, softWrap: true, overflow: TextOverflow.fade,)
+                        ),
+                    ],
+                  ),
+                  builder: (_, collapsed, expanded) {
+                    return Padding(
+                      padding: EdgeInsets.only(left: 10, right: 10, bottom: 10),
+                      child: Expandable(
+                        collapsed: collapsed,
+                        expanded: expanded,
+                        theme: ExpandableThemeData(crossFadePoint: 0),
+                      ),
+                    );
+                  },
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       )
